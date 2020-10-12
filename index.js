00bed4024ffce9da4fb42c445140b5cfe038102e
nikita <freenikita95@gmail.com>
2020-10-12 18:23:07 +0300

    change some module and connect to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose')
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -8,15 +10,13 @@ app.get('/', (req, res) => {
     res.send("ok")
 })
 
-const MONGO_CONNECT = "mongodb+srv://nikita:JaB5sqmHVoyLzfaT@cluster0-boop9.mongodb.net/restAPI?retryWrites=true&w=majority"
-
 async function start(){
     try {
-        // await mongoose.connect(MONGO_CONNECT, {
-        //     useNewUrlParser: true,
-        //     useFindAndModify: false,
-        //     useUnifiedTopology: true
-        // })
+        await mongoose.connect(process.env.BD_HOST, {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        })
 
         app.listen(PORT, () => {
             console.log(`The server was run on port ${PORT}`)
