e20d559509a7b53c122bd50980678bfe8b76a22d
nikita <freenikita95@gmail.com>
2020-10-14 15:39:26 +0300

    new setting

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+require('dotenv').config();
 
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-    res.send("ok")
-})
-
-async function start(){
-    try {
-        await mongoose.connect(process.env.BD_HOST, {
-            useNewUrlParser: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true
-        })
-
-        app.listen(PORT, () => {
-            console.log(`The server was run on port ${PORT}`)
-        })
-
-    } catch(error) {
-        console.log("Error Run Project: ", error )
-    }
-}
-
-start()
+module.exports = require(`./${process.env.NODE_ENV === 'production' ? 'build' : 'src'}/app.js`);
