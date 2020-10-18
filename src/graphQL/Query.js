5c4006d36dd1a7602e8fe1e876eef5e0dd8a7030
nikita <freenikita95@gmail.com>
2020-10-18 17:13:21 +0300

    set user from mutation t query

--- src/graphQL/Query.js
+++ src/graphQL/Query.js
@@ -1,12 +1,31 @@
 import Wallet from "./../modules/wallet";
+import User from "../modules/user";
 
 export const typeDefsQuery = `
     type Query {
+        setUser(firebase_id: String): User
         getAllWallets(user_id: String): [Wallet]
   }
 `
 
 export const resolversQuery = {
+    setUser: async (_,{firebase_id}) => {
+        try {
+            let user = await User.findOne({
+                firebase_id
+            })
+
+            if(user) return user
+
+            user = await new User({
+                firebase_id,
+            })
+
+            return await user.save()
+        } catch (err) {
+            console.log('setUser: ', err)
+        }
+    },
     getAllWallets: async (_, params) => {
         try {
             const { user_id } = params;
