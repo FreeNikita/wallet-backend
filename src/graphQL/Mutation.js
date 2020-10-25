47ac1122364322919837ea60f3b4ff189dfa7a24
nikita <freenikita95@gmail.com>
2020-10-26 00:51:08 +0200

    add mutation setUser

--- src/graphQL/Mutation.js
+++ src/graphQL/Mutation.js
@@ -3,6 +3,7 @@ import Wallet from '../modules/wallet'
 
 export const typeDefsMutation = `
     type Mutation {
+        setUser(firebase_id: String!): User
         createWallet(
             user_id: String,
             name: String,
@@ -55,6 +56,23 @@ export const typeDefsMutation = `
 // Move to USER_MODULES
 
 export const resolversMutation = {
+    setUser: async (_,{firebase_id}) => {
+        try {
+            let user = await User.findOne({
+                firebase_id
+            })
+
+            if(user) return user
+
+            user = await new User({
+                firebase_id,
+            })
+
+            return await user.save()
+        } catch (err) {
+            console.log('setUser: ', err)
+        }
+    },
     createWallet: async (_, params) => {
         try {
             const {user_id, name = '', amount = 0, currency = '', type = '' } = params;
